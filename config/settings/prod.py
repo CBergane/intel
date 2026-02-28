4cc14613ab71642a1cd8c1bac33794e40896c4fb
Christian Bergane <christian.bergane@gmail.com>
2026-02-28 11:26:27 +0100

    Fix trailing slashes + consistent login errors

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -8,11 +8,6 @@ if not os.getenv("SECRET_KEY") or SECRET_KEY == "replace-this-in-production" or 
     raise RuntimeError(
         "SECRET_KEY must be set in environment and at least 32 characters in production."
     )
-    
-if "test" in sys.argv:
-    SECURE_SSL_REDIRECT = False
-    SESSION_COOKIE_SECURE = False
-    CSRF_COOKIE_SECURE = False
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = env_bool("SECURE_SSL_REDIRECT", True)
@@ -26,3 +21,9 @@ SECURE_HSTS_PRELOAD = False
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = "DENY"
+
+    
+if "test" in sys.argv:
+    SECURE_SSL_REDIRECT = False
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
